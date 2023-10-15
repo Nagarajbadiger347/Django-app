34e9bc75b006c0215f9a2b28a7694f98b203b67c
Nagaraj Badiger <nagarajbadiger347@gmail.com>
2023-10-15 16:23:59 +0530

    modified project included test case

---
+++ hello_world/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+
+class HelloWorldTestCase(TestCase):
+    def test_hello_world(self):
+        url = reverse('home')  # Use the name of your view function, which is 'home'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello world!!! this is my capstone project......")
